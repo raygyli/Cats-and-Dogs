2917155cdd049337cbb29c29a0f511e3e31833e2
Ray Li <seriouscoder123@gmail.com>
2024-01-18 22:40:48 +0800

    Update model

--- model.py
+++ model.py
@@ -9,12 +9,13 @@ def build_model(input_shape):
 
     X = Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), padding='valid')(inputs)
     X = ReLU()(X)
-    X = Conv2D(filter=64, kernel_size=(3, 3), strides=(1, 1), padding='valid')(X)
-    X = ReLU()(X)
-    X = MaxPooling2D(pool_size=(2, 2), strides=1)
+    # X = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='valid')(X)
+    # X = ReLU()(X)
+    X = MaxPooling2D(pool_size=(2, 2), strides=2)(X)
     X = Flatten()(X)
-    X = Dense(X.shape[1], activation='relu')
+    flatten_shape = tf.reduce_prod(X.shape[1:])
+    X = Dense(flatten_shape, activation='relu')(X)
 
     outputs = Dense(1, activation='sigmoid')(X)
-    model = Model(input=inputs, outputs=outputs)
+    model = Model(inputs=inputs, outputs=outputs)
     return model
