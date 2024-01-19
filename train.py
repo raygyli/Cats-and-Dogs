0b7f304bbda86a8b4cf5c9c87e44d181ba97e7b6
Ray Li <seriouscoder123@gmail.com>
2024-01-19 21:34:49 +0800

    Split data into train, valid and test sets

--- train.py
+++ train.py
@@ -38,5 +38,10 @@ if __name__ == "__main__":
     # Visualize
     visualize_data(train_data, config["batch_size"])
 
+    # Split data
+    X_train, X_tmp, y_train, y_tmp = train_test_split(train_data, test_size=0.4, random_state=42)
+    X_valid, X_test, y_valid, y_test = train_test_split(X_tmp, y_tmp, test_size=0.5, random_state=42)
+    del X_tmp, y_tmp
+
     model = build_model(tuple(config["input_shape"]))
     hist = train(model, )
