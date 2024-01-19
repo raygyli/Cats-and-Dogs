cf794afcd49cbee2ec934156627fd773207ecce8
Ray Li <seriouscoder123@gmail.com>
2024-01-19 21:28:52 +0800

    Add train.py

---
+++ train.py
@@ -0,0 +1,42 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+
+import logging
+import tensorflow as tf
+tf.get_logger().setLevel(logging.ERROR)
+
+from data_loader import load_data
+from model import build_model
+from utils import *
+
+def train(model, X_train, y_train, num_epochs=100, optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"]):
+    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+    history = model.fit(X_train, y_train, epochs=num_epochs)
+    return history
+
+if __name__ == "__main__":
+    # Set TensorFlow GPU memory allocation mode
+    physical_devices = tf.config.list_physical_devices('GPU')
+    try:
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    except RuntimeError as e:
+        print(e)
+
+    # Open configurations
+    with open("config.json", 'r') as config_file:
+        config = json.load(config_file)
+
+    # Load data
+    data, info = load_data("./data")
+    train_data = data["train"]
+    train_data = train_data.map(preprocess).batch(config["batch_size"]).shuffle(1000)
+    train_data = train_data.prefetch(tf.data.AUTOTUNE)
+
+    # Visualize
+    visualize_data(train_data, config["batch_size"])
+
+    model = build_model(tuple(config["input_shape"]))
+    hist = train(model, )
