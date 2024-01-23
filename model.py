2431657807bde668def63e979ffccc06fb201fa7
Ray Li <seriouscoder123@gmail.com>
2024-01-23 16:08:08 +0800

    IDK wat i did

--- model.py
+++ model.py
@@ -7,10 +7,8 @@ from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense,
 def build_model(input_shape):
     inputs = Input(shape=input_shape)
 
-    X = Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), padding='valid')(inputs)
+    X = Conv2D(filters=5, kernel_size=(3, 3), strides=(1, 1), padding='valid')(inputs)
     X = ReLU()(X)
-    # X = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='valid')(X)
-    # X = ReLU()(X)
     X = MaxPooling2D(pool_size=(2, 2), strides=2)(X)
     X = Flatten()(X)
     flatten_shape = tf.reduce_prod(X.shape[1:])
